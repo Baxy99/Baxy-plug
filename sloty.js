997fad8a5bb0aac5b7e6debd5ce3b9879785a5e2
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-09-14 15:13:04 +0200

    Żeby nie było update sloty.js
    Zmiana wersji tego niesamowitego dzieła zbiorowej pracy najlepszych programistów z całego świata

--- sloty.js
+++ sloty.js
@@ -1,5 +1,5 @@
 console.log("Lepsze niż kurwa bot");
-API.chatLog("Po chuj makro na sloty v1.3");
+API.chatLog("Po chuj makro na sloty v1.1.2");
 API.sendChat('!slot');
 setInterval(function () {
     API.sendChat('!slot');
