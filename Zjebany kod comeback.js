d1b537c1d5a190ed852b34e7bc058755876bb842
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-09-15 18:44:47 +0200

    Update Zjebany kod comeback.js

--- Zjebany kod comeback.js
+++ Zjebany kod comeback.js
@@ -2,20 +2,8 @@ console.log("To nie dziala jakbym chcial i jak powinno. Wiem o tym. I kurwa gown
 API.chatLog("Zjebane autosloty z którymi jednak da sie wygrać  v1.0retarded. Agresywny console.log");
 API.sendChat('!slot');
 setInterval(function () {
-    API.getWaitListPosition = function (id) {
-        if (typeof id === 'undefined' || id === null) {
-            id = API.getUser().id;
-        }
-        var wl = API.getWaitList();
-        for (var i = 0; i < wl.length; i++) {
-            if (wl[i].id === id) {
-                return i;
-            }
-        }
-        return -1;
-    };
     var dj = API.getDJ().id;
-    var pos = API.getWaitListPosition(id);
+    var pos = API.getWaitListPosition(user.id);
     if (dj === chat.uid || pos == 0) {
         return void(0);
     } else {
